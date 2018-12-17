086ae28323e0d4742cfbcae05c3d36df06364628
Eric Cascketta <caskman@gmail.com>
2018-12-17 07:49:05 -0600

    better summary

--- go.py
+++ go.py
@@ -1,7 +1,8 @@
 from openpyxl import Workbook
 import re
 import csv
-from pprint import pprint
+from datetime import datetime
+from operator import attrgetter, itemgetter
 
 def read_debit(data, label):
     with open(f"csv/{label}.csv", "r", newline='') as fin:
@@ -43,6 +44,8 @@ def clean_data(data):
     for d in data:
         amt = re.sub(r"\"", "", d.amt)
         d.amt = float(amt)
+
+        d.date = datetime.strptime(d.date, "%m/%d/%Y").date()
     return data
 
 
@@ -80,14 +83,14 @@ def main():
 
     incomews.title = "Income"
     incomews.append(["Date", "Description", "Amount"])
-    for d in income_data:
+    for d in sorted(income_data, key=attrgetter("date"), reverse=True):
         incomews.append([d.date, d.desc, d.amt])
     incomews.append(["", "", f"=SUM(C2:C{incomews.max_row})"])
     income_sum_addr = f"{incomews.title}!C{incomews.max_row}"
 
     expensews.title = "Expenses"
     expensews.append(["Date", "Description", "Amount"])
-    for d in expense_data:
+    for d in sorted(expense_data, key=attrgetter("date"), reverse=True):
         expensews.append([d.date, d.desc, d.amt])
     expensews.append(["", "", f"=SUM(C2:C{expensews.max_row})"])
     expense_sum_addr = f"{expensews.title}!C{expensews.max_row}"
@@ -95,8 +98,35 @@ def main():
     summaryws.title = "Summary"
     summaryws.append(["Income", f"={income_sum_addr}"])
     summaryws.append(["Expense", f"={expense_sum_addr}"])
-    summaryws.append(["Net", f"=B1-B2"])
+    summaryws.append(["Net", f"=B1+B2"])
 
+    months_dict = {}
+    def months_key(date_obj):
+        return datetime.strptime(f"{date_obj.year}-{date_obj.month}", "%Y-%m").date()
+    for d in data:
+        key = months_key(d.date)
+        month_obj = None
+        if key in months_dict:
+            month_obj = months_dict[key]
+        else:
+            month_obj = Month(key, [],[])
+            months_dict[key] = month_obj
+        if d.amt > 0:
+            month_obj.income.append(d)
+        else:
+            month_obj.expense.append(d)
+
+    summaryws.append([])
+    summaryws.append(["Date", "Income", "Expense", "Net"])
+    months_list = [(k, months_dict[k]) for k in months_dict]
+    months_list = sorted(months_list, key=itemgetter(0), reverse=True)
+    for month_t in months_list:
+        date = month_t[0]
+        month = month_t[1]
+        income = sum(map(lambda x: x.amt, month.income))
+        expense = sum(map(lambda x: x.amt, month.expense))
+        row = summaryws.max_row + 1
+        summaryws.append([date, income, expense, f"=B{row}+C{row}"])
     
     wb.save("output.xlsx")
 
@@ -114,6 +144,11 @@ class Transaction():
     def __str__(self):
         return self.__repl__()
 
+class Month():
+    def __init__(self, date, income, expense):
+        self.date = date
+        self.income = income
+        self.expense = expense
 
 
 main()
