695b6f3134ac53d882f4c53db5b87428257cdb3d
Eric Cascketta <caskman@gmail.com>
2019-01-28 18:45:11 -0600

    changed id to utc time

--- go.py
+++ go.py
@@ -1,7 +1,7 @@
-import uuid
+import datetime
 import os
 
-from s3datasource import download_data, upload_output
+from s3datasource import download_data
 from envvars import get_var
 from spreadsheet import build_spreadsheet
 from datainput import get_data
@@ -9,7 +9,7 @@ from datainput import get_data
 TEMP_DIR = get_var("TEMP_DIR")
 
 def main():
-    run_id = uuid.uuid1()
+    run_id = datetime.datetime.utcnow().replace(microsecond=0).isoformat()
     temp_dir = f"{TEMP_DIR}-{run_id}"
 
     if not os.path.exists(temp_dir):
