06cdd8943101e40ec7a17637fbcdc98c868c05df
Eric Cascketta <caskman@gmail.com>
2020-01-03 15:15:53 -0600

    publish

---
+++ doc/precache-manifest.50b14fba73d3120f39ad5a2223de3e8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e8aa638c980a04652aa42a3aac258d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73f3672c05790b9350b0",
+    "url": "/static/css/main.d60a0de9.chunk.css"
+  },
+  {
+    "revision": "923cc2b8cac41bf3d09a",
+    "url": "/static/js/2.491adaf7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.491adaf7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "73f3672c05790b9350b0",
+    "url": "/static/js/main.bfc9cf27.chunk.js"
+  },
+  {
+    "revision": "4d57832a98cc9e844219",
+    "url": "/static/js/runtime-main.fc2af53b.js"
+  }
+]);
